3cba7f6c5563d018e66174bb2492f87501f1901e
phoom <s6604062620212@email.kmutnb.ac.th>
2024-07-23 20:38:40 +0700

    test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ app.use('/auth',require('./routers/auth.route'))
 app.use('/item',require('./routers/item.route'))
 
 
+
 app.listen(process.env.SERVER_PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT || 3000}`);
 })
